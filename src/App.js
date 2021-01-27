510f35409da0414cb171ed7f259e5ed20c90f090
Aaron Lichtblau <al27@princeton.edu>
2021-01-26 22:39:49 -0500

    homepage design full

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import './App.css';
-import React, {useState, render} from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Container, Row, Col} from 'react-bootstrap';
 
 
 function App() {
   return (
     <div className="app">
-    <div className="welcomeText">
-    <h2>Name Goes Here</h2>
-    <p>tagline tagline tagline...</p>
-    </div>
-    <br></br>
-    <div className="imageCarousel">
-    <ControlledCarousel></ControlledCarousel>
-    </div>
+    <Container>
+      <Row>
+        <Col></Col>
+        <Col>
+        <div className="welcomeText">
+        <h2>Name Goes Here</h2>
+        <p>tagline tagline tagline...</p>
+        </div>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <br></br>
+        <Col></Col>
+        <Col>
+        <div className="imageCarousel">
+        <ControlledCarousel></ControlledCarousel>
+        </div>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col>
+        <h3>Challenge Board</h3>
+        </Col>
+        <Col>
+        <h3>Leader Board</h3>
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 }
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
+  var aaronPic = require('./pics/Aaron.png');
+  var ezraPic = require('./pics/Ezra.png');
+
+  const picStyle = {
+    float:'center',
+    height:'300px',
+    margin: 'auto',
+    padding: '10px'
+  }
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
@@ -28,46 +59,35 @@ function ControlledCarousel() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
+      <div>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
+          src={aaronPic.default}
+          style={picStyle}
           alt="First slide"
         />
+        </div>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+      <div>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          src={ezraPic.default}
+          style={picStyle}
           alt="Second slide"
         />
-
+        </div>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
     </Carousel>
   );
 }
-
-render(<ControlledCarousel />);
 
 export default App;
