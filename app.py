562e10c462fb61e432b346f13e842e0302c8a778
Aaron Lichtblau <al27@princeton.edu>
2021-01-26 19:56:35 -0500

    added flask backend

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, jsonify, session
 import os
+from flask import Flask, jsonify
 
-app = Flask(__name__, static_folder='./build',static_url_path='/')
+app = Flask(__name__,static_folder='./build',static_url_path='/')
 
-#-------------------------------------------------------------------------------
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
+
 #-------------------------------------------------------------------------------
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
